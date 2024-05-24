78bde3cd081f57210e4ddfc08afd3800dc6fb049
HOAN-C <hoan.c9907@gmail.com>
2024-05-24 06:00:25 +0000

    Updated HTML payload

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,8 +11,8 @@ cat << EOM > /var/www/html/index.html
   <div style="width:800px;margin: 0 auto">
 
   <!-- BEGIN -->
-  # <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
-  <img alt="이쁜사진멋진사진" style="width:100%" src="https://www.sejong.go.kr/crosseditor/crosseditor_3.5.0.04_prog/binary/images/000127/20160704160925969_UYBHRKP4.jpg"/>
+  <img alt="이쁜사진멋진사진" style="width:100%" src="https://almondine-range-25b.notion.site/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2F376407bd-4f03-4fbe-ad73-bb74f309a569%2F2ca46ef9-75b8-4bc6-9425-96e12970e795%2FUntitled.jpeg?table=block&id=5681eeba-131e-42ff-a259-7333e9da2927&spaceId=376407bd-4f03-4fbe-ad73-bb74f309a569&width=1420&userId=&cache=v2" />
+  <img alt="이쁜사진멋진사진" style="width:100%" src="https://almondine-range-25b.notion.site/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2F376407bd-4f03-4fbe-ad73-bb74f309a569%2F28a93efd-e5b7-4981-99f7-13b603203a1b%2FUntitled.jpeg?table=block&id=11f14572-5167-40f9-bf2d-c6c26daf495d&spaceId=376407bd-4f03-4fbe-ad73-bb74f309a569&width=1420&userId=&cache=v2" />
   <div>First PR on TERRAFORM</div>
   <center><h2>Meow World!</h2></center>
   Welcome to ${PREFIX}'s app. Replace this text with your own.
